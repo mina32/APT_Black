3bb726820ede3996dfeba7204be8433f6785b2ed
mina32 <carmina_francia32@yahoo.com>
2017-10-18 03:47:04 -0500

    first commit

---
+++ apt_proj/connexus-app/app/src/main/java/com/appspot/v2_dot_apt_black_app/connexus_app/NearbyActivity.java
@@ -0,0 +1,13 @@
+package com.appspot.v2_dot_apt_black_app.connexus_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NearbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby);
+    }
+}
---
+++ apt_proj/connexus-app/app/src/main/java/com/appspot/v2_dot_apt_black_app/connexus_app/SingleStreamActivity.java
@@ -0,0 +1,13 @@
+package com.appspot.v2_dot_apt_black_app.connexus_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleStreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_stream);
+    }
+}
